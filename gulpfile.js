b23830f82354ec805e8bf4b05e0ccfa2abf8686b
Pedro <pmsalinas@gmail.com>
2017-04-20 16:52:44 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,9 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function(){
-console.log("bravo creaste una tarea gulp");
-});
 
-gulp.task('html', function(){
-console.log("imagine algo espectacular dreado para html");
-});
 
-gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars, nested, autoprefixer]))
-		.pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch', function(){
 
-	watch('./app/index.html', function(){
 
-		gulp.start('html');
 
-	});
-
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('styles');
-	});
-
-});
